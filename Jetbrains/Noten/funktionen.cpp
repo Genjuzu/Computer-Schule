fbd6078bd52dd6e170dd5768b8374bf46a7311e1
Wudu <kikonekoyuki@gmail.com>
2024-08-29 10:50:56 +0200

    Nu

---
+++ Jetbrains/Noten/funktionen.cpp
@@ -0,0 +1,5 @@
+//
+// Created by CSL on 29.08.2024.
+//
+
+#include "funktionen.h"
---
+++ Jetbrains/Noten/funktionen.h
@@ -0,0 +1,8 @@
+//
+// Created by CSL on 29.08.2024.
+//
+
+#ifndef FUNKTIONEN_H
+#define FUNKTIONEN_H
+
+#endif //FUNKTIONEN_H
