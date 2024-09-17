36c557ea6f10c2bd76affe45174c793517f6015d
Wudu <kikonekoyuki@gmail.com>
2024-09-17 12:19:35 +0200

    Nu

---
+++ Jetbrains/jmnhhhgj/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    int b = 4;
+    int i;
+    float c = a/b;
+
+    cout << c++ << endl << endl << endl;
+
+    for(i = 0; i < 15; i++) {
+        cout << i << endl;
+    }
+    cout << endl << i << endl;
+
+    return 0;
+}
