c33b2741cd22d9279d0c002cf656d97f3814f8ad
Wudu <149156275+Genjuzu@users.noreply.github.com>
2024-09-30 10:18:27 +0200

    psh

---
+++ Übung/STRUCT/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string.h>
+#include <vector>
+
+using namespace std;
+
+
+int main()
+{
+    struct person {
+        char vorname[20] = "";
+        char nachname[20] = "";
+        int id;
+    };
+
+    int main() {
+
+        char vn[] = "Eduard";
+        person edi;
+        edi
+        strcpy(edi.vorname, vn);
+        char nn[] = "Kampf";
+        strcpy(edi.nachname, nn);
+        edi.id = 1;
+
+    }
+}
