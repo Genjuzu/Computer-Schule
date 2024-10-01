e594105012ae3fb620ec4d19e44f4582fd4a7830
Wudu <149156275+Genjuzu@users.noreply.github.com>
2024-10-01 12:52:22 +0200

    puiszh

--- Übung/STRUCT/main.cpp
+++ Übung/STRUCT/main.cpp
@@ -1,27 +1,29 @@
 #include <iostream>
-#include <string.h>
 #include <vector>
-
+#include <string>
 using namespace std;
+
+
+struct Karte {
+    string name;
+    string farbe; // Farbe der Karte (Pik, Herz, Karo, Kreuz)
+    int wert;     // Wert der Karte (2-10 für Zahlen, 10 für Bildkarten, 1 oder 11 für Ass)
+};
+
+
 
 
 int main()
 {
-    struct person {
-        char vorname[20] = "";
-        char nachname[20] = "";
-        int id;
-    };
 
-    int main() {
+    karte pik2, Pik3, Pik4, Pik5, Pik6, Pik7, Pik8, Pik9, PikBube, PikDame, PikKoenig, PikAss;
 
-        char vn[] = "Eduard";
-        person edi;
-        edi
-        strcpy(edi.vorname, vn);
-        char nn[] = "Kampf";
-        strcpy(edi.nachname, nn);
-        edi.id = 1;
 
-    }
+
+    pik2.name = "Pik 2";
+    pik2.wert= 2;
+
+
+
+
 }
